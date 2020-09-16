fe41e049030563bf896aedc2ed52a4f4cb3cafc2
kcl4167 <kcl4167@126.com>
2020-09-16 17:03:40 +0800

    seventh commit

--- palindrome.py
+++ palindrome.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
-s = input("Please enter a string:  ")
-z = s[::-1]
-if s == z:
-    print("The string is a palindrome")
-else:
-    print("The string is not a palindrome")
+def palindrome(s):
+    return s == s[::-1]
+if __name__ == '__main__':
+    s = input("Enter a string: ")
+    if palindrome(s):
+        print("Yay a palindrome")
+    else:
+        print("Oh no, not a palindrome")
