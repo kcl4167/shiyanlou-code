c246300b58809234cd1992655ebcdf25f1aee88e
kcl4167 <kcl4167@126.com>
2020-09-17 15:14:24 +0800

    seventh commit

---
+++ MinutesToHours.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+import sys
+
+def Hours(minute):
+    if minute < 0:
+        raise ValueError("Input number cannot be negative")
+    else:
+        print("{} H, {} M".format(minute//60, minute%60))
+
+try:
+    Hours(int(sys.argv[1]))
+except:
+    print("parameter Error")
